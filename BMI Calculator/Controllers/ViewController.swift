56dcd48889efc873f48109faa876843bb7ac3e1c
Jeff Deng <dengh@oregonstate.edu>
2022-11-19 22:26:17 -0800

    Added slider buttons + actions

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))Kg"
+        print("weight: \(sender.value)")
+    }
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightLabel.text = "\(round(100 * sender.value)/100)m"
+        print("height: \(sender.value)")
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
 }
 
